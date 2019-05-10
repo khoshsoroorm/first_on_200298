2772fc93441cbd13e983139726dbb940cd37b312
khoshsoroorm <khoshsoroorm@gmail.com>
2019-05-10 12:31:56 +0330

    type_of_variable

---
+++ example_list.py
@@ -0,0 +1,17 @@
+lst1_test = [3, 66, 787, 33, 6666, 7677]
+# mamoli
+print(lst1_test)
+
+# ye slice az list
+print(lst1_test[1:4])
+
+# az yeja ta akhar bargardone
+print(lst1_test[4:])
+
+# yeki dar mion
+print(lst1_test[0: :2])
+
+
+#makos print kone
+test_test = 'malas'
+print(test_test[::-1])
