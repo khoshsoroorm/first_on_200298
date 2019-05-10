08336276beb09cd10b4ecc2109d935a95200f786
khoshsoroorm <khoshsoroorm@gmail.com>
2019-05-10 11:59:50 +0330

    type_of_variable

---
+++ types_of_variable.py
@@ -0,0 +1,25 @@
+num_test = 100
+print(num_test, type(num_test))
+
+float_test = 12.6
+print(float_test, type(float_test))
+
+complex_test = 2+5j
+print(complex_test, type(complex_test))
+
+bool_test = True
+print(bool_test, type(bool_test))
+
+string_test = "first test of string"
+print(string_test, type(string_test))
+
+list_test = [1, 2, 4, 56]
+len_test = len(list_test)
+print(list_test, type(list_test))
+print('len of the list is', len_test)
+
+tuple_test = (1, 44, 66, 45)
+print(tuple_test, type(tuple_test))
+
+
+
